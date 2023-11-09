6e3514d150f808dd0352131840d414b8ccd7462f
1uanss <sousasluan212@gmail.com>
2023-11-09 08:15:53 -0300

    renomiando arquivos

--- app/Views/fase1.php
+++ app/Views/fase1.php
@@ -93,8 +93,7 @@
             }
         });
     </script>
-    <script src="<?= base_url('assets/luan/script.js'); ?>"></script>
-    <script src="public/assets/luan/jquery.js"></script>
+    <script src="<?= base_url('assets/js/script.js'); ?>"></script>
     <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
     <!-- <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
     <script nomodule src="https://unpkg/ .com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script> -->
---
+++ public/assets/js/script.js
@@ -0,0 +1,79 @@
+const input = document.getElementById('input-value');
+const botao = document.getElementById('salvar');
+
+
+let arraySobreTempoPresionadoDasTeclas = [];
+let arrayEntreTempoDasTeclas = [];
+const teclasPressionadas = [];
+const indexLetras = {};
+let totalLetrasApertadas = 0;
+
+function aperta(letra){
+    teclasPressionadas.push({letra, apertou: Date.now()});
+    indexLetras[letra] = teclasPressionadas.length - 1;
+}
+
+function solta(letra){
+    const index = indexLetras[letra];
+    teclasPressionadas[index].soltou = Date.now();
+    delete indexLetras[letra];
+}
+
+window.onload = function(){
+    input.addEventListener('keydown',e=>{
+        console.log('press', e.key);
+        aperta(e.key);
+    })
+    
+    input.addEventListener('keyup',e=>{
+        solta(e.key);
+    })
+    
+    botao.addEventListener('click',(e)=>{
+        e.preventDefault()
+        compara()
+        document.querySelector('#inputSobreTempoPresionadoDasTeclas').value = `${arraySobreTempoPresionadoDasTeclas.toString()}`;
+        document.querySelector('#inputEntreTempoDasTeclas').value = `${arrayEntreTempoDasTeclas.toString()}`;
+        // console.log(document.querySelector('#inputEntreTempoDasTeclas').value)
+        // console.log(document.querySelector('#inputSobreTempoPresionadoDasTeclas').value)
+    });
+    
+}
+
+function compara(){
+    console.log(teclasPressionadas)
+    
+    teclasPressionadas.forEach((element, idx) => {
+        const apertouAtual = element.apertou;
+        const soltouAtual = element.soltou;
+
+
+
+
+        let nextElement;
+        let apertouProximo;
+        let soltouProximo;
+        let tempoPressionando;
+        let tempoEntreTeclas;
+        
+        
+        if(idx+1 < teclasPressionadas.length) {
+             nextElement = teclasPressionadas[idx+1]
+             apertouProximo = nextElement.apertou;
+             soltouProximo = nextElement.soltou;
+             tempoEntreTeclas = apertouProximo - soltouAtual;
+             arrayEntreTempoDasTeclas.push(tempoEntreTeclas);
+            }
+        tempoPressionando = soltouAtual - apertouAtual;
+        arraySobreTempoPresionadoDasTeclas.push(tempoPressionando);
+        
+        console.log(element.letra,{tempoPressionando, tempoEntreTeclas});
+
+        // console.log(arraySobreTempoPresionadoDasTeclas);
+        // console.log(arrayEntreTempoDasTeclas);
+    });
+
+    const senha = (teclasPressionadas.map((elm)=>elm.letra)).join('')
+    // console.log({senha})
+    
+}
